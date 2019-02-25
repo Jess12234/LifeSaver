28730b0e000dac9fe245f4dc982dd6bbf841ba4a
Jessica Lowry <jdl14a@lib-301-06-9114.local>
2019-02-25 08:24:32 -0600

    Initial Commit

---
+++ LifeSaver/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LifeSaver
+//
+//  Created by Jessica Lowry on 2/25/19.
+//  Copyright © 2019 Jessica Lowry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
